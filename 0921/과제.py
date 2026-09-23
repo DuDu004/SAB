47dc84429131d6f9e6636388700f60bbcec5ceb1
DuDu004 <duddnchl1@naver.com>
2026-09-23 02:01:56 +0000

    1 commit

---
+++ 0921/과제.py
@@ -0,0 +1,13 @@
+List = [list(map(int,input().split())) for _ in range(4)]
+A = List[0]
+B = List[1]
+C = List[2]
+D = List[3]
+
+def favorite():
+    for i in range:
+        flag = [0, 0, 0 ,0]
+        if A[i] == 4:
+            flag[i] = 1
+        if B[i] == 4 or C[i] == 4 or D[i] == 4:
+            
