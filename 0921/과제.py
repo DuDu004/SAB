95a72bde6aadf7a8daef5ae37d75fcb0c8587548
DuDu004 <duddnchl1@naver.com>
2026-09-23 09:26:48 +0000

    1_1 commit

--- 0921/과제.py
+++ 0921/과제.py
@@ -1,13 +1,26 @@
-List = [list(map(int,input().split())) for _ in range(4)]
-A = List[0]
-B = List[1]
-C = List[2]
-D = List[3]
+def favorite(List, n, flag):
+    if n > 3:
+        return 0
 
-def favorite():
-    for i in range:
-        flag = [0, 0, 0 ,0]
-        if A[i] == 4:
-            flag[i] = 1
-        if B[i] == 4 or C[i] == 4 or D[i] == 4:
-            
+    most = 0
+
+    for i in range(4):
+        if flag[i] == 1:
+            continue
+        flag[i] = 1
+        
+        sum = List[n][i] + favorite(List, n+1, flag)
+        if sum > most:
+            most = sum
+
+        flag[i] = 0
+
+    return most
+
+
+List = [list(map(int,input().split())) for _ in range(4)]
+n = 0
+flag = [0, 0, 0, 0]
+
+print(favorite(List, n, flag))
+
